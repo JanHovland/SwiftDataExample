5afb462aecc1a1ea0aa9039fecf6a5e39eefe0b0
JanHovland <37158506+JanHovland@users.noreply.github.com>
2024-09-04 11:35:26 +0200

    Initial Commit

---
+++ SwiftDataExample/SwiftDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExampleApp.swift
+//  SwiftDataExample
+//
+//  Created by Jan Hovland on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
