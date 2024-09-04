91891aad87b89a1a2ed16ab044dd7a911fe52f43
JanHovland <37158506+JanHovland@users.noreply.github.com>
2024-09-04 18:18:08 +0200

    Update

--- SwiftDataExample/SwiftDataExample.swift
+++ SwiftDataExample/SwiftDataExample.swift
@@ -1,24 +1,145 @@
 //
-//  ContentView.swift
+//  SwiftDataExample.swift
 //  SwiftDataExample
 //
 //  Created by Jan Hovland on 04/09/2024.
 //
 
 import SwiftUI
+import SwiftData
 
-struct ContentView: View {
+struct SwiftDataExample: View {
+    
+    /// Intro to SwiftData - Model, Container, Fetch, Create, Update & Delete
+    /// https://www.youtube.com/watch?v=mvXFGikltPc
+    
+    @Environment(\.modelContext) var content
+    @State private var isShowingItemSheet = false
+    @Query(sort: \Expense.date) var expenses: [Expense]
+    @State private var expenseToEdit: Expense?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(expenses) { expense in
+                    ExpenseCell(expense: expense)
+                        .onTapGesture {
+                            expenseToEdit = expense
+                        }
+                }
+                .onDelete { indexSet in
+                    for index in indexSet {
+                        content.delete(expenses[index])
+                    }
+                }
+            }
+            .navigationTitle("Expenses")
+            .navigationBarTitleDisplayMode(.large)
+            .sheet(isPresented: $isShowingItemSheet) {
+                AddExpenseSheet()
+            }
+            .sheet(item: $expenseToEdit) { expense in
+                UpdateExpenseSheet(expense: expense)
+            }
+            .toolbar {
+                if !expenses.isEmpty {
+                    Button("Add Expense", systemImage: "plus") {
+                        isShowingItemSheet = true
+                    }
+                }
+            }
+            .overlay {
+                if expenses.isEmpty {
+                    ContentUnavailableView(label: {
+                        Label("No expenses", systemImage: "list.bullet.rectangle.portrait")
+                    },  description: {
+                        Text("Start adding expenses to see your list.")
+                    },  actions: {
+                        Button("Add Expense") {
+                            isShowingItemSheet = true
+                        }
+                    })
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ExpenseCell: View {
+    
+    let expense: Expense
+    
+    var body: some View {
+        HStack {
+            Text(expense.date, format: .dateTime.month(.abbreviated).day())
+            Text(expense.name)
+            Spacer()
+            Text(expense.value, format: .currency(code: "NO"))
+            
+        }
+    }
 }
+
+struct AddExpenseSheet: View {
+    
+    @Environment(\.modelContext) var context
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name: String = ""
+    @State private var date: Date = .now
+    @State private var value: Double = 0.00
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Expense", text: $name)
+                DatePicker("Date", selection: $date, displayedComponents: .date)
+                TextField("Value", value: $value, format: .currency(code: "NO"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("New Expense")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button("Save") {
+                        let expense = Expense(name: name, date: date, value: value)
+                        context.insert(expense)
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct UpdateExpenseSheet: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @Bindable var expense: Expense
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $expense.name)
+                DatePicker("Date", selection: $expense.date, displayedComponents: .date)
+                TextField("Value", value: $expense.value, format: .currency(code: "NO"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Update Expense")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button("Done") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
--- SwiftDataExample/SwiftDataExampleApp.swift
+++ SwiftDataExample/SwiftDataExampleApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct SwiftDataExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SwiftDataExample()
         }
+        .modelContainer(for: [Expense.self])
     }
 }
